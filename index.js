bbfb096aa7ab9c3f32c224d15a201cae3ddb62ea
Nick <nick.skelin@gmail.com>
2019-09-30 12:21:58 -0700

    Logout button moves user to login page. Login checks firebase for existing user before proceeding to homepage. Signup adds users to firebase db.

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public'));
 
+//db setup
+var firebase = require('firebase/app');
+require('firebase/auth');
+var firebaseConfig = {
+	apiKey: "AIzaSyBvCy3aVXijlVRYzBAjk-SELvDxjtYB0qQ",
+	authDomain: "what-eats.firebaseapp.com",
+	databaseURL: "https://what-eats.firebaseio.com",
+	projectId: "what-eats",
+	storageBucket: "what-eats.appspot.com",
+	messagingSenderId: "460494922666",
+	appId: "1:460494922666:web:4521dd967a85392e"
+};
+firebase.initializeApp(firebaseConfig);
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./private-admin-key.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://what-eats.firebaseio.com"
+});
+var db = admin.firestore();
 
 app.get('/', (req, res) => {
     res.render('landingPage.hbs');
@@ -21,12 +44,34 @@ app.get('/', (req, res) => {
 
 app.post('/signup', (req, res) => {
 	console.log(req.body);
-	res.redirect('/homePage');
+	let email = req.body.login;
+	console.log(email);
+	let password = req.body.password;
+	firebase.auth().createUserWithEmailAndPassword(email, password)
+	.then(() => {
+		res.redirect('/homePage');
+	})
+	.catch((error) => {
+		var errorCode = error.code;
+  		var errorMessage = error.message;
+  		console.log(errorCode);
+		console.log(errorMessage);
+	})
 });
 
 app.post('/login', (req, res) => {
-	console.log(req.body);
-	res.redirect('/homePage');
+	let email = req.body.login;
+	let password = req.body.password;
+	firebase.auth().signInWithEmailAndPassword(email, password)
+	.then(() => {
+		res.redirect('/homePage');
+	})
+	.catch((error) => {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		console.log(errorCode);
+		console.log(errorMessage);
+	});
 });
 
 app.get('/homePage', (req, res) => {
@@ -36,6 +81,10 @@ app.get('/homePage', (req, res) => {
 app.get('/customisePage', (req, res) => {
     res.render('customiseFoodPage.hbs');
 });
+
+app.get('/logout', (req, res) => {
+	res.redirect('/');
+})
 
 app.post('/addDish', (req, res) => {
 	//save to database
